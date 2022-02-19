b5fd4b9f8e7f16b51e7cb08b819e8c57cff38ab5
pc <pc@DESKTOP-TQGQV41>
2022-02-18 20:44:52 -0600

    Ejercicio 1,2,3.

--- Edad de Usuario/Program.cs
+++ Edad de Usuario/Program.cs
@@ -6,13 +6,41 @@ namespace Edad_de_Usuario
     {
         static void Main(string[] args)
         {
-           
-            Console.WriteLine("Ingrese su Edad: ");
-            int edad = int.Parse(Console.ReadLine());
-            Console.WriteLine("No aparentas " + edad  +  "años");
-            Console.ReadKey();
-            
-            
+
+            //Console.WriteLine("Ingrese su Edad: ");
+            //byte edad = byte.Parse(Console.ReadLine());
+            //Console.WriteLine("No aparentas " + edad + "años");
+            //Console.ReadKey();
+
+            //byte n1, n2;
+            //ushort result;
+            //Console.WriteLine("Ingrese Primer Número: ");
+            //n1 = byte.Parse(Console.ReadLine());
+            //Console.WriteLine("Ingrese Segundo Número: ");
+            //n2 = byte.Parse(Console.ReadLine());
+
+            //result = Convert.ToUInt16(n1 * n2);
+
+            //Console.WriteLine("El resultado es: "+ result);
+
+            long n1, n2, sum, res, pro;
+            Console.WriteLine("Ingrese Primer Número: ");
+            n1 = long.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese Segundo Número: ");
+            n2 = long.Parse(Console.ReadLine());
+            sum = n1 + n2;
+            res = n1 - n2;
+            pro = n1 * n2;
+
+            Console.WriteLine("El resultado de la suma es: " + sum);
+            Console.WriteLine("El resultado de la resta es: " + res);
+            Console.WriteLine("El resultado del producto es: " + pro);
+
+
+
+
+
+
         }
     }
 }
