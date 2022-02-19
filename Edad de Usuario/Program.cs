db1a384034f05818ceca79eec8fa5a0ba0034241
pc <pc@DESKTOP-TQGQV41>
2022-02-18 20:12:25 -0600

    inicio

---
+++ Edad de Usuario/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Edad_de_Usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Ingrese su Edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("No aparentas " + edad  +  "años");
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
